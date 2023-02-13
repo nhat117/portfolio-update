58970ec49bd2ee7067004b3fed70329f68ab74a9
nhat117 <s3878174@rmit.edu.vn>
2023-02-13 13:00:26 +0700

    Commit skil section

---
+++ components/Skill.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+type Props = {
+  directionLeft?: boolean;
+};
+
+const Skill = (props: Props) => {
+  return (
+    <div className='relative flex cursor-pointer group'>
+      <motion.div
+        initial={{ x: props.directionLeft ? -200 : 200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ x: 0, opacity: 1 }}
+      >
+        <div className='relative w-24 h-24 transition duration-300 duration xl:w-32 xl:h-32 filter group-hover:grayscale'>
+          <Image
+            className='object-cover w-full h-full border border-gray-500 rounded-full'
+            alt='firebase'
+            width={50}
+            height={50}
+            src='https://user-images.githubusercontent.com/72547907/218379528-7ef8ba85-15bf-4813-9911-63578b4bfb18.png'
+          />
+        </div>
+      </motion.div>
+      <div className="absolute z-0 w-24 h-24 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:opacity-80 group-hover:bg-white xl:w-32 xl:h-32">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-300">70%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Skill from './Skill'
 type Props = {}
 
 const Skills = (props: Props) => {
   return (
-    <motion.div className='relative flex flex-col justify-center mx-auto items-center  text-center md:text-left xl:flex-row max-w-[2000px] xl:px-1 xl:space-y-0 min-h-screen'>
+    <motion.div 
+        initial={{ opacity: 0}}
+        whileInView={{ opacity: 1}}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+        viewport={{ once: true }}
+        className='relative flex flex-col justify-center mx-auto items-center  text-center md:text-left xl:flex-row max-w-[2000px] xl:px-1 xl:space-y-0 min-h-screen'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
+        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for currency proficiency</h3>
+        {/* A div contains a grid pattern */}
+        <div className='grid grid-cols-4 gap-5'>
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            
+        </div>
     </motion.div>
   )
 }
